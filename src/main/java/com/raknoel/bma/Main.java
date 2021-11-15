af39d0e7f2645c1d5aae31960efe7a77b4f8a686
RakNoel <Oskar.leir@gmail.com>
2021-11-15 07:53:53 +0100

    Getting there 410 hrs of testing in latest tests

--- src/main/java/com/raknoel/bma/Main.java
+++ src/main/java/com/raknoel/bma/Main.java
@@ -17,60 +17,54 @@ public class Main {
 
     public static void main(String[] args) {
         BigQuery bigquery = BigQueryOptions.getDefaultInstance().getService();
-        int width = 100, height = 20;
+        int width = 300, height = 20;
+        int h = 100;
+        int r = 120;
 
-        for (int h = 1; h <= 3; h++)
-            r:
-            for (int r = 2; r <= 3; r++)
-                for (int k = 1; k <= 30; k++)
-                    for (int i = 0; i < 30; i++) {
-                        int d = new Random().nextInt(2 * k) + 1;
-                        Timestamp timestamp = new Timestamp(System.currentTimeMillis());
-                        System.out.printf(
-                                "Running: WH:%dx%d, R:%d, H:%d, K:%d, D:%d, I:%d %n",
-                                width, height, r, h, k, d, i
-                        );
+        for (int k = 100; k < 200; k++) {
+            int d = new Random().nextInt(2 * k) + 1;
+            Timestamp timestamp = new Timestamp(System.currentTimeMillis());
+            System.out.printf(
+                    "Running: WH:%dx%d, R:%d, H:%d, K:%d, D:%d, I:%d %n",
+                    width, height, r, h, k, d, k
+            );
 
-                        var binaryMatrix = MatrixGenerator
-                                .generateRKMatrix(width, height, d, h, new Random().nextInt());
+            var binaryMatrix = MatrixGenerator
+                    .generateRKMatrix(width, height, d, h, new Random().nextInt());
 
-                        Map<String, Object> content = new HashMap<>();
-                        content.put("k", k);
-                        content.put("r", r);
-                        content.put("d", d);
-                        content.put("h", h);
-                        content.put("matrix_width", width);
-                        content.put("matrix_height", height);
-                        content.put("computer_id", "DESKTOP_1");
-                        content.put("rundate", SIMPLE_DATE_FORMAT.format(timestamp));
+            Map<String, Object> content = new HashMap<>();
+            content.put("k", k);
+            content.put("r", r);
+            content.put("d", d);
+            content.put("h", h);
+            content.put("matrix_width", width);
+            content.put("matrix_height", height);
+            content.put("computer_id", "DESKTOP_1");
+            content.put("rundate", SIMPLE_DATE_FORMAT.format(timestamp));
 
-                        final long START = System.currentTimeMillis();
-                        try {
-                            BMA solver = new BMA(binaryMatrix, k, r);
-                            var sol = solver.Approximate();
+            final long START = System.currentTimeMillis();
+            try {
+                BMA solver = new BMA(binaryMatrix, k, r);
+                var sol = solver.Approximate();
 
-                            content.put("solved", true);
-                            content.put("best_k", binaryMatrix.getChild().totalHammingDist(sol.getCenters()));
-                            content.put("best_r", sol.getWidth());
-                        } catch (Exception e) {
-                            content.put("solved", false);
-                            content.put("best_k", -1);
-                            content.put("best_r", -1);
-                        } finally {
-                            final long STOP = System.currentTimeMillis();
-                            final long RUNTIME = STOP - START;
-                            content.put("runtime_ms", RUNTIME);
+                content.put("solved", true);
+                content.put("best_k", binaryMatrix.getChild().totalHammingDist(sol.getCenters()));
+                content.put("best_r", sol.getWidth());
+            } catch (Exception e) {
+                content.put("solved", false);
+                content.put("best_k", -1);
+                content.put("best_r", -1);
+            } finally {
+                final long STOP = System.currentTimeMillis();
+                final long RUNTIME = STOP - START;
+                content.put("runtime_ms", RUNTIME);
 
-                            var res = insertAll(bigquery, content);
-                            if (res.hasErrors()) {
-                                res.getInsertErrors().forEach((x, y) -> System.out.println(y.toString()));
-                            }
-
-                            if (RUNTIME > 5000) {
-                                continue r;
-                            }
-                        }
-                    }
+                var res = insertAll(bigquery, content);
+                if (res.hasErrors()) {
+                    res.getInsertErrors().forEach((x, y) -> System.out.println(y.toString()));
+                }
+            }
+        }
     }
 
     public static InsertAllResponse insertAll(BigQuery bq, Map<String, Object> rowContent) {
